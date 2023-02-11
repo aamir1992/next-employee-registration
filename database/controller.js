c2f89d48bc18e336d1b98f2f99ecf9cd3ecca1f4
Mohammed Aamir <m.aamir@domain.rage-india.com>
2023-02-11 22:25:56 +0530

    Controller changes6

--- database/controller.js
+++ database/controller.js
@@ -17,7 +17,7 @@ export async function getUsers(req, res){
 // get : http://localhost:3000/api/users/1
 export async function getUser(req, res){
     try {
-        const { userId } = req.params.id;
+        const { userId } = req;
 
         if(userId){
             const user = await Users.findById(userId);
